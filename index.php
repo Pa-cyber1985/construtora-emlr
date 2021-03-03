18b7a8b72a5db8ffb47343c51f3887d9bc8b2dab
Patricia Nogueira <correiodapaty@hotmail.com>
2021-03-03 16:20:12 -0300

    trabalhando com cores no html

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
    <body>
       <h1>Construtora EMLR</h1>
       <p>Obras e reformas em geral</p>
-      <div style="width:100%;display:flex;heigh: 80px">
+      <div style="width:100%;display:flex;height: 80px">
          <div style="width:50%;background:blue"></div>
          <div style="width:50%;background:red"></div>
       </div>
